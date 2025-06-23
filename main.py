3ae4b10f66c8b949fbfddcda60b6cb6bc08b75f5
Leander Mitsch <leander.mitsch@nussbaum-medien.de>
2025-06-23 18:18:55 +0200

    Renamed casing of Database.py, added pathlib library to unify path to database

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import Kurs, Database
+import Kurs, Database, pathlib
 
 if __name__ == "__main__":
     kurs1 = Kurs.Kurs("Deutsch", "Herr Ruhl", 1)
-    db = Database.Database(".StudiService-3000-/test.db")
+    db = Database.Database(pathlib.Path("test.db"))
     cur  = db.connection.cursor()
 
     print(db.read("SELECT * FROM kurs"))
